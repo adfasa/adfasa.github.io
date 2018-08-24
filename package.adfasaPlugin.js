24219147ff48a8d334ac3f3fd72dad4419fc32d1
Tumurbaatar <adfasa@gmail.com>
2018-08-24 12:19:31 +0800

    Update package.adfasaPlugin.js

--- package.adfasaPlugin.js
+++ package.adfasaPlugin.js
@@ -25,12 +25,12 @@ define("plugins/adfasaPlugin/__static__",[], function(require, exports, module) 
                 {
                     "type": "snippets",
                     "filename": "html.snippets",
-                    "data": "# scope: html\nsnippet fre\n\t<?php foreach($${1:result} as $${2:${3:key}=>$${4:row}}): ?>\n\t\t${5:#source}\n\t<?php endforeach; ?>\n\t\nsnippet php\n\t<?php ${1:code} ?>\n\t\nsnippet eco\n\t<?php echo ${1}; ?>\n\t\nsnippet iff\n\t<?php if(${1:condition}): ?>\n\t\t${2:#source}\n\t<?php endif; ?>\n\t\nsnippet ife\n\t<?php if(${1:condition}): ?>\n\t\t${2:#source}\n\t<?php else: ?>\n\t\t${3:#source}\n\t<?php endif; ?>"
+                    "data": "# scope: html\nsnippet fre\n\t<?php foreach($${1:result} as $${2:${3:key}=>$${4:row}}): ?>\n\t\t${5:#source}\n\t<?php endforeach; ?>\nsnippet php\n\t<?php ${1:code} ?>\nsnippet eco\n\t<?php echo ${1} ?>\nsnippet iff\n\t<?php if(${1:condition}): ?>\n\t\t${2:#source}\n\t<?php endif; ?>\nsnippet ife\n\t<?php if(${1:condition}): ?>\n\t\t${2:#source}\n\t<?php else: ?>\n\t\t${3:#source}\n\t<?php endif; ?>"
                 },
                 {
                     "type": "snippets",
                     "filename": "javascript.snippets",
-                    "data": "# scope: javascript\nsnippet log\n\tconsole.log(${1:\"${2}\"})$0\n\t\nsnippet ajax\n\t$.ajax({\n\t\ttype: \"${1}\",\n\t\turl: \"${2}\",\n\t\tdata: {${3}},\n\t\tsuccess: function(res){\n\t\t\t${4}\n\t\t}\n\t})\n\t\nsnippet ready\n\t$(function(){\n\t\n\t\t${1://Goodluck!}\n\t\t\n\t});\n\t\nsnippet click\n\t$(\"${1}\").click(function(e){\n\t\t${2}\n\t});"
+                    "data": "# scope: javascript\nsnippet log\n\tconsole.log(${1:\"${2}\"})$0\nsnippet ajax\n\t$.ajax({\n\t\ttype: \"${1}\",\n\t\turl: \"${2}\",\n\t\tdata: {${3}},\n\t\tsuccess: function(res){\n\t\t\t${4}\n\t\t}\n\t})\nsnippet ready\n\t$(function(){\n\t\n\t\t${1://Goodluck!}\n\t\t\n\t});\nsnippet click\n\t$(\"${1}\").click(function(e){\n\t\t${2}\n\t});"
                 },
                 {
                     "type": "snippets",
